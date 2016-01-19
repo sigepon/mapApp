feee786e1ecd645f88cff1ccae226ff941c4da59
Kikuchi Shigeo <K@sigepon.local>
2016-01-19 16:00:39 +0900

    initial commit

---
+++ mapApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  mapApp
+//
+//  Created by Kikuchi Shigeo on 2016/01/18.
+//  Copyright © 2016年 sigepon. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.showsUserLocation = true
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2DMake(39.703531, 141.152667)
+//        pin.title = name
+        mapView.addAnnotation(pin)
+    }
+
+}
+
